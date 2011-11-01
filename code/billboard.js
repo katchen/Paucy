11857a81c3f667eb1e7416d177bd55f07060b79b
Emilio Lopez <emilio@rockmelt.com>
2011-11-01 03:06:07 -0700

    take one

--- code/billboard.js
+++ code/billboard.js
@@ -6,62 +6,29 @@ const WEIGHTS =
 
 var data = {}
 
-/*var w = 960,
-    h = 700,
-    r = Math.min(w, h) / 2,
-    color = d3.scale.category20c();
+var w = 960,
+	h = 800,
+	r = Math.min(w, h) / 2, //radius
+	color = d3.scale.ordinal()
+		.range(["#DD2858", "#328768", "#67A2C9", "#204EE2", "#9C67C6", "#9CD8BF", "#DB3BBD", "#7E2230", "#CC9BB2", "#CAC765", "#DCEBCC", "B7B2B5"]);
+		
+var vis = d3.select("#record").append("svg:svg")
+	.attr("width", w)
+	.attr("height", h)
+	.append("svg:g")
+	.attr("transform", "translate(" + w / 2 + "," + h / 2 +")");
 
-var vis = d3.select("#chart").append("svg:svg")
-	.attr("class" "sunburst")
-    .attr("width", w)
-    .attr("height", h)
-  .append("svg:g")
-    .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")");
-
+// Recursively partition node tree into sunburst
 var partition = d3.layout.partition()
-    .sort(null)
-    .size([2 * Math.PI, r * r])
-    .value(function(d) { return 1; });
-
+	.sort(null) // Default is descending order by associated input data's numeric value attribute
+	.size([2 * Math.PI, r * r])
+	.value(function(d) { return 1; });
+	
 var arc = d3.svg.arc()
-    .startAngle(function(d) { return d.x; })
-    .endAngle(function(d) { return d.x + d.dx; })
-    .innerRadius(function(d) { return Math.sqrt(d.y); })
-    .outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });
-
-d3.json("../data/flare.json", function(json) {
-  var path = vis.data([json]).selectAll("path")
-      .data(partition.nodes)
-    .enter().append("svg:path")
-      .attr("display", function(d) { return d.depth ? null : "none"; }) // hide inner ring
-      .attr("d", arc)
-      .attr("fill-rule", "evenodd")
-      .style("stroke", "#fff")
-      .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
-      .each(stash);
-
-  d3.select("#size").on("click", function() {
-    path
-        .data(partition.value(function(d) { return d.size; }))
-      .transition()
-        .duration(1500)
-        .attrTween("d", arcTween);
-
-    d3.select("#size").classed("active", true);
-    d3.select("#count").classed("active", false);
-  });
-
-  d3.select("#count").on("click", function() {
-    path
-        .data(partition.value(function(d) { return 1; }))
-      .transition()
-        .duration(1500)
-        .attrTween("d", arcTween);
-
-    d3.select("#size").classed("active", false);
-    d3.select("#count").classed("active", true);
-  });
-});
+	.startAngle(function(d) { return d.x; })
+	.endAngle(function(d) { return d.x+ d.dx; })
+	.innerRadius(function(d) { return Math.sqrt(d.y); })
+	.outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });
 
 // Stash the old values for transition.
 function stash(d) {
@@ -78,7 +45,7 @@ function arcTween(a) {
     a.dx0 = b.dx;
     return arc(b);
   };
-}*/
+}
 
 // Get Data
 
@@ -158,6 +125,13 @@ d3.csv("billboardHot.csv", function(csvData)
 			};
 		}
 	}
-	console.log(data['1990']);
+	var path = vis.data([data['2010']]).selectAll("path") // Selects all elements that match selector string
+		.data(partition.nodes)
+		.enter().append("svg:path")
+		.attr("display", function(d) { return d.depth ? null : "none"; }) // hide inner ring
+		.attr("d", arc)
+		.attr("fill-rule", "evenodd")
+		.style("stroke", "#fff")
+		.style("fill", function(d) { return color((d.children ? d : d.parent).name); });
 });
 
